f86a854b993e7d98b932d1334790dc25d9c65fd4
Muhammad Umair <91285533+RanaUmair000@users.noreply.github.com>
2024-12-12 07:02:41 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
   int f,s;
   cout<<"Enter First Number"<<endl;
-  cin>>f;  
+  cin>>f;
   cout<<"Enter Second Number"<<endl;
   cin>>s;
   cout<<"Hello World"<<endl;  
