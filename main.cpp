863c0c958cd98da99ae58c90464df474e72049b0
Muhammad Umair <91285533+RanaUmair000@users.noreply.github.com>
2025-01-01 05:23:15 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
   int f,s;
   cout<<"Enter First Number"<<endl;
   cin>>f;
-  cout<<"Enter Second Number"<<endl;
+  cout<<"Enter Second Number"<<endl;  
   cin>>s;  
   cout<<"Hello World"<<endl;  
 }
