78330c8d942b9a941f30b3f6e1fdf0687503fb8e
Muhammad Umair <91285533+RanaUmair000@users.noreply.github.com>
2025-01-10 10:21:08 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
   int f,s;
-  cout<<"Enter First Number"<<endl;
+  cout<<"Enter First Number"<<endl;  
   cin>>f;
   cout<<"Enter Second Number"<<endl;  
   cin>>s;  
